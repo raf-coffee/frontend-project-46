8aa6e6e316587ab652dae34f521cc141485f5707
raf_coffee <pavel.hexlet@mail.ru>
2023-05-04 22:46:00 +0300

    refactor

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,44 +3,26 @@ import resultPlain from '../__fixtures__/resultPlain.js';
 import resultJSON from '../__fixtures__/resultJSON.json';
 import showDiff from '../src/index.js';
 
-describe('stylish formatter', () => {
-  test('with json', () => {
-    expect(showDiff('file1.json', 'file2.json', 'stylish')).toEqual(resultStylish);
-  });
-
-  test('with yaml', () => {
-    expect(showDiff('file1.yaml', 'file2.yaml', 'stylish')).toEqual(resultStylish);
-  });
-
-  test('with yml', () => {
-    expect(showDiff('file1.yml', 'file2.yml', 'stylish')).toEqual(resultStylish);
-  });
+test('compare stylish with json', () => {
+  expect(showDiff('file1.json', 'file2.json', 'stylish')).toEqual(resultStylish);
 });
 
-describe('plain formatter', () => {
-  test('with json', () => {
-    expect(showDiff('file1.json', 'file2.json', 'plain')).toEqual(resultPlain);
-  });
-
-  test('with yaml', () => {
-    expect(showDiff('file1.yaml', 'file2.yaml', 'plain')).toEqual(resultPlain);
-  });
-
-  test('with yml', () => {
-    expect(showDiff('file1.yml', 'file2.yml', 'plain')).toEqual(resultPlain);
-  });
+test('compare stylish with yaml/yml', () => {
+  expect(showDiff('file1.yml', 'file2.yml', 'stylish')).toEqual(resultStylish);
 });
 
-describe('json formatter', () => {
-  test('with json', () => {
-    expect(showDiff('file1.json', 'file2.json', 'json')).toEqual(resultJSON);
-  });
+test('compare plain with json', () => {
+  expect(showDiff('file1.json', 'file2.json', 'plain')).toEqual(resultPlain);
+});
 
-  test('with yaml', () => {
-    expect(showDiff('file1.yaml', 'file2.yaml', 'json')).toEqual(resultJSON);
-  });
+test('compare plain with yaml/yml', () => {
+  expect(showDiff('file1.yml', 'file2.yml', 'plain')).toEqual(resultPlain);
+});
 
-  test('with yml', () => {
-    expect(showDiff('file1.yml', 'file2.yml', 'json')).toEqual(resultJSON);
-  });
+test('compare json with json', () => {
+  expect(showDiff('file1.json', 'file2.json', 'json')).toEqual(resultJSON);
+});
+
+test('compare json with yaml/yml', () => {
+  expect(showDiff('file1.yml', 'file2.yml', 'json')).toEqual(resultJSON);
 });
