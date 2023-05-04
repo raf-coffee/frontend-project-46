2dbeaba94d2014012559eafc5674c9241a3ac204
raf_coffee <pavel.hexlet@mail.ru>
2023-05-04 17:04:11 +0300

    small fixes

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -1,8 +1,7 @@
-import stringify from "../../utils/stringify.js";
+import stringify from '../../utils/stringify.js';
 
-const currentIndent = (treeDepth, status = 'not unchanged', replacer = " ", spaces = 4) => (
+const currentIndent = (treeDepth, status, replacer = ' ', spaces = 4) => (
   replacer.repeat(status !== 'unchanged' ? spaces * treeDepth - 2 : spaces * treeDepth));
-
 
 const formatter = (tree) => {
   const iter = (node, depth) => {
@@ -15,12 +14,12 @@ const formatter = (tree) => {
           return `${currentIndent(depth, 'unchanged')}${value.key}: ${iter(value.value, depth + 1)}`;
         }
         if (value.status === 'added') {
-          return `${currentIndent(depth)}+ ${value.key}: ${iter(value.value, depth + 1)}`;
+          return `${currentIndent(depth, 'added')}+ ${value.key}: ${iter(value.value, depth + 1)}`;
         }
         if (value.status === 'deleted') {
-          return `${currentIndent(depth)}- ${value.key}: ${iter(value.value, depth + 1)}`;
+          return `${currentIndent(depth, 'deleted')}- ${value.key}: ${iter(value.value, depth + 1)}`;
         }
-        return `${currentIndent(depth)}- ${value.key}: ${iter(value.value1, depth + 1)}\n${currentIndent(depth)}+ ${value.key}: ${iter(value.value2, depth + 1)}`;
+        return `${currentIndent(depth, 'changed')}- ${value.key}: ${iter(value.value1, depth + 1)}\n${currentIndent(depth, 'changed')}+ ${value.key}: ${iter(value.value2, depth + 1)}`;
       });
       return stringify(lines, depth);
     }
--- utils/stringify.js
+++ utils/stringify.js
@@ -1,5 +1,3 @@
-const braceIndent = (treeDepth, replacer = " ", spaces = 4) => replacer.repeat(spaces * treeDepth - spaces);
+const braceIndent = (treeDepth, replacer = ' ', spaces = 4) => replacer.repeat(spaces * treeDepth - spaces);
 
-export default (lines, depth) => {
-  return ['{', ...lines, `${braceIndent(depth)}}`].join('\n');
-}
+export default (lines, depth) => ['{', ...lines, `${braceIndent(depth)}}`].join('\n');
