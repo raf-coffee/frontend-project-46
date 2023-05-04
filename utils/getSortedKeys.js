c58529f99122fc76245da1b2e1c57acc97e8c21e
raf_coffee <pavel.hexlet@mail.ru>
2023-05-04 23:05:25 +0300

    refactor

--- utils/getSortedKeys.js
+++ utils/getSortedKeys.js
@@ -1,4 +1,3 @@
-
 // eslint-disable-next-line fp/no-mutating-methods
 export default (obj1, obj2) => Array.from(new Set(Object.keys(obj1).concat(Object.keys(obj2))))
   .sort((a, b) => a.localeCompare(b));
