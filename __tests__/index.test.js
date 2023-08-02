1a6ac6474a4972d52d5465b746b105d2a92f09e0
raf_coffee <pavel.hexlet@mail.ru>
2023-08-02 15:47:43 +0300

    fix linter errors

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,45 +10,45 @@ const getFixturesPaths = (extension) => {
   const fileBefore = resolvePath(`__fixtures__/file1.${extension}`);
   const fileAfter = resolvePath(`__fixtures__/file2.${extension}`);
   return [fileBefore, fileAfter];
-}
+};
 
 describe('Formatters', () => {
   describe('Without formatter', () => {
-    test.each(extensions)("with %s", (extension) => {
+    test.each(extensions)('with %s', (extension) => {
       const [fileBefore, fileAfter] = getFixturesPaths(extension);
       const actual = showDiff(fileBefore, fileAfter);
       expect(actual).toEqual(resultStylish);
-    })
+    });
   });
 
   describe('Stylish formatter', () => {
-    test.each(extensions)("with %s", (extension) => {
+    test.each(extensions)('with %s', (extension) => {
       const [fileBefore, fileAfter] = getFixturesPaths(extension);
       const actual = showDiff(fileBefore, fileAfter, 'stylish');
       expect(actual).toEqual(resultStylish);
-    })
+    });
   });
 
   describe('Plain formatter', () => {
-    test.each(extensions)("with %s", (extension) => {
+    test.each(extensions)('with %s', (extension) => {
       const [fileBefore, fileAfter] = getFixturesPaths(extension);
       const actual = showDiff(fileBefore, fileAfter, 'plain');
       expect(actual).toEqual(resultPlain);
-    })
+    });
   });
 
   describe('JSON formatter', () => {
-    test.each(extensions)("with %s", (extension) => {
+    test.each(extensions)('with %s', (extension) => {
       const [fileBefore, fileAfter] = getFixturesPaths(extension);
       const actual = showDiff(fileBefore, fileAfter, 'json');
       expect(actual).toEqual(resultJSON);
-    })
+    });
   });
 
   describe('Wrong formatter', () => {
-    test.each(extensions)("should throw an error with %s", (extension) => {
+    test.each(extensions)('should throw an error with %s', (extension) => {
       const [fileBefore, fileAfter] = getFixturesPaths(extension);
       expect(() => showDiff(fileBefore, fileAfter, 'js')).toThrow(Error);
-    })
+    });
   });
-})
+});
